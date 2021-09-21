6458e2ab4c2dc8148b09c975676be170088efd93
hazwanizafirah <hazwanizafirah78@gmail.com>
2021-09-21 11:21:26 +0800

    Remember Me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save {self.email = email.downcase}
   validates :name, presence: true
@@ -8,11 +9,10 @@ class User < ApplicationRecord
   has_secure_password
   before_save :downcase_email
 
-  private
-
-  def downcase_email
-    self.email.downcase!
-  end
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
 
   # Returns the hash digest of the given string.
   def User.digest string
@@ -23,4 +23,25 @@ class User < ApplicationRecord
     end
       BCrypt::Password.create string, cost: cost
   end
+end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
+
 end
